d996c7f9367c2c05d98daa616323bccd2a916672
Paulo Duque <paulo.duque@terra.com.br>
2022-05-31 09:01:49 -0300

    Continuando PHP 3105

---
+++ modulo_5/aula_03/index.php
@@ -0,0 +1,30 @@
+<?php
+    $nome = ' Paulo Duque';
+    echo 'Meu nome é: <br  />';
+    echo $nome;
+    echo '<br>';
+    echo '<br>';
+    $nome = ' Paulo José Terra Duque:';
+    echo 'Meu nome é: <br  />';
+    echo $nome;
+    
+    echo '<br>';
+    echo '<br>';
+    
+    $nome = ' Paulo Duque';
+    $idade = 61;
+    echo 'Meu nome é: <br  />';
+    echo $nome; 
+    echo  '<br>';
+    echo 'Minha idade é: <br  />';
+    echo $idade;
+
+
+
+
+
+    // echo $_SERVER['SERVER_NAME'];
+    // echo $_SERVER['DOCUMENT_ROOT'];
+    // echo '<pre>';
+    // print_r($_SERVER);
+    // echo '</pre>';
