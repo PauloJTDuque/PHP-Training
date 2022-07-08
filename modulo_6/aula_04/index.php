3666e188b4374b600bd2b7a7faad2490ee1c6226
Paulo Duque <paulo.duque@terra.com.br>
2022-07-08 08:33:04 -0300

    PHP 0807

--- modulo_6/aula_04/index.php
+++ modulo_6/aula_04/index.php
@@ -4,6 +4,7 @@
         echo '<h2>Nome eh: </h2>' .$nome;
         echo'<hr>';
         echo '<h2>Idade eh: </h2>' .$idade;
+        echo'<hr>';
         
     }
     mostrarnome("Paulo Terra",61);
@@ -14,6 +15,14 @@
     }
     calculadora(20,30); // Se  num1 ou num2 não for passado, ele utilizará 10 para num1 e 20 para num2
 
-    
+    function verdade(){
+        return true
+    }
+
+    function retornarString(){
+        echo 'Paulo José Terra Duque';
+    }
+
+    echo retornarString(); 
 
 ?>
