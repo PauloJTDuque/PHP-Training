691ed91097452e9f930e5f19cf101f14788e3122
Paulo Duque <paulo.duque@terra.com.br>
2022-07-22 08:36:00 -0300

    PHP Training 2207A

---
+++ modulo_6/aula_07/index.php
@@ -0,0 +1,16 @@
+<?php
+
+$nome = 'Paulo';
+
+switch($nome){
+    case 'Paulo';
+        echo 'Minha Variável é Paulo ' ;
+        break;
+    case 'Duque';
+        echo 'Minha Variável é Duque ' ;
+        break;
+};
+
+// Break pode ser usado em loop for, while, do while, foreach e switch;
+
+?>
