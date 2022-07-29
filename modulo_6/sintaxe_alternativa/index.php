ccfa9f264aa37607d5ad256ce937f1777bfa3ec7
Paulo Duque <paulo.duque@terra.com.br>
2022-07-29 10:35:15 -0300

    PHP Training 2907A

---
+++ modulo_6/sintaxe_alternativa/index.php
@@ -0,0 +1,26 @@
+<?php
+
+$nome='Paulo José Terra Duque';
+
+if($nome=='Paulo José Terra Duque'):
+    echo 'Verdade';
+endif;    
+
+$contador = 0;
+
+while($contador<10):
+    $contador++;
+    echo 'Oi';
+    echo '<br />';
+endwhile;    
+
+$array = array();
+foreach ($array as $key => $value):
+
+endforeach;
+
+for($i=0;$i<10;$i++):
+
+endfor;    
+
+?>
