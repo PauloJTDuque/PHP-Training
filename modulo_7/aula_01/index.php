b71a1a9f458115f14c8606620fea0b20fc5e04ca
Paulo Duque <paulo.duque@terra.com.br>
2022-07-29 11:00:27 -0300

    PHP Training 2907B

---
+++ modulo_7/aula_01/index.php
@@ -0,0 +1,27 @@
+<?php
+    class Pessoa{
+        // Objeto Pessoa
+
+        private $nome = 'Paulo Duque';                  //Declarado como private
+        private $idade = '61';                                //tudo fica disponível apenas dentro da classe;
+        private $peso = '70kg';
+
+        public function crescer(){
+            $this->comer();                                     // qdo quero trabalhar com o método dentro da classe
+            echo 'Estou Crescendo';
+        }
+        
+        private function comer(){
+            echo 'Estou Comendo';                           // uso "this->"
+        }
+
+    }
+
+    // Instanciar
+    $pessoa = new Pessoa;
+    $pessoa2 = new Pessoa;
+
+    $pessoa->crescer();  // tracinho ->(chevron) para acessar os métodos
+
+
+?>
